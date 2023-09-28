3da6d26a9b51a9efbf827f85bd797d6cea5f4a39
Jivak Sute <jivakysute@gmail.com>
2023-09-28 21:01:53 +0530

    m6-fix

--- index.js
+++ index.js
@@ -4,20 +4,11 @@ const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const blogRoutes = require("./routes/blogRoutes");
 const { connection } = require("./configs/db");
-const { createProxyMiddleware } = require("http-proxy-middleware");
-
 require("dotenv").config();
 const app = express();
 
 app.use(cors());
-app.use(
-  "/api",
-  createProxyMiddleware({
-    target: "https://eager-sunbonnet-bass.cyclic.cloud",
-    changeOrigin: true,
-  })
-);
-app.use(express.static("public"));
+//
 app.use(express.json());
 app.use(bodyParser.json());
 
